8b53ef3700c40a446246a0baffd157bcde696df5
MarcelMG <28888720+MarcelMG@users.noreply.github.com>
2020-05-04 15:06:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# usb_hid_snes_gamepad
+ Building a SNES Gamepad to USB Adapter using STM32F103 microcontroller
---
+++ Release/Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/usbd_hid.c 
+
+OBJS += \
+./Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/usbd_hid.o 
+
+C_DEPS += \
+./Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/usbd_hid.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/usbd_hid.o: ../Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/usbd_hid.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Inc -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Middlewares/ST/STM32_USB_Device_Library/Core/Inc -I../Middlewares/ST/STM32_USB_Device_Library/Class/HID/Inc -O3 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/usbd_hid.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
